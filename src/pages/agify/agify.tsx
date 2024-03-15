46d300ba635bfb3a390128e9013ceba0c3e97a68
Yurii Semenov <ysx080@gmail.com>
2024-03-15 18:41:56 +0500

    refactor: age custom fetch hook

--- src/pages/agify/agify.tsx
+++ src/pages/agify/agify.tsx
@@ -2,55 +2,39 @@ import { Button, Div, FormItem, Group, Input, Spinner } from "@vkontakte/vkui";
 import { ComponentProps, useEffect, useState } from "react";
 import { useAgeStatus } from "src/shared/model/age-status";
 import styles from "./style.module.css";
+import { useCheckAge } from "src/shared/api/check-age";
 
-type Agify = {
-  count: number
-  name: string
-  age: number
-}
 
-export function Agify(props : ComponentProps<typeof Group>) {
+export function Agify(props: ComponentProps<typeof Group>) {
+
+  let delayInputTimeoutId: NodeJS.Timeout;
 
   const [name, setName] = useState<string>('')
-  const [age, setAge] = useState<number>()
-  const [isLoading, setIsLoading] = useState(false)
-  const [error, setError] = useState<string>()
   const [isDebouncing, setIsDebouncing] = useState(false)
 
-  const ageStatus = useAgeStatus(age)
+  const [age, fetchAge] = useCheckAge(name)
+  const ageStatus = useAgeStatus(age.data)
 
   useEffect(() => {
-    if(!name) {
+    if (!name) {
       setIsDebouncing(false)
+      fetchAge()
       return;
     }
     setIsDebouncing(true)
-    const delayInputTimeoutId = setTimeout(() => {
-      checkAge();
+    delayInputTimeoutId = setTimeout(() => {
+      fetchAge()
       setIsDebouncing(false)
     }, 3000);
     return () => clearTimeout(delayInputTimeoutId);
   }, [name]);
 
-  const checkAge = async () => {
-    if (isLoading) return
-    setAge(undefined);
-    setIsLoading(true);
-    try {
-      const data = await fetch(`https://api.agify.io?name=${name}`, {cache: "force-cache"}).then<Agify>(res => res.json())
-      setAge(data.age)
-    }
-    catch (error) {
-      console.error(error)
-      setError("Ошибка получения возраста, попробуйте ещё раз.")
-    }
-    setIsLoading(false);
-  }
-
   return (<Group id="tab-content-agify" aria-labelledby="tab-agify" role="tabpanel" {...props}>
     <form onSubmit={(e) => {
       e.preventDefault();
-      checkAge()
+      clearTimeout(delayInputTimeoutId)
+      setIsDebouncing(false)
+      fetchAge()
     }}>
       <FormItem htmlFor="agify-input"
         top="Введите имя">
@@ -59,15 +43,15 @@ export function Agify(props : ComponentProps<typeof Group>) {
           value={name}
           onChange={e => setName(e.target.value)}
           after={
-            <Spinner style={{visibility:  isDebouncing ? 'visible' : 'hidden'}}/>
+            <Spinner style={{ visibility: isDebouncing ? 'visible' : 'hidden' }} />
           }
         />
       </FormItem>
       <FormItem
-        status={error ? 'error' : 'default'}
-        bottom={error}>
-        <Button type="submit" size="l" stretched disabled={isLoading || !name}>
-          {isLoading ? <Spinner /> : "Проверить возраст"}
+        status={age.error ? 'error' : 'default'}
+        bottom={age.error}>
+        <Button type="submit" size="l" stretched disabled={age.isLoading || !name}>
+          {age.isLoading ? <Spinner /> : "Проверить возраст"}
         </Button>
       </FormItem>
 
---
+++ src/shared/api/check-age.tsx
@@ -0,0 +1,50 @@
+import { useState } from "react"
+
+type AgeCache = {
+    [key: string]: number
+}
+
+type AgifyResponse = {
+    count: number
+    name: string
+    age: number
+}
+
+export function useCheckAge(name: string) {
+    const ageCache: AgeCache = {}
+    let controller: AbortController;
+
+    const [age, setAge] = useState<number>()
+    const [isLoading, setIsLoading] = useState(false)
+    const [error, setError] = useState<string>()
+
+    const refetch = async () => {
+        if (controller) controller.abort()
+        controller = new AbortController()
+        setAge(undefined);
+        setIsLoading(true);
+
+        try {
+            if (!name) {
+                return
+            }
+
+            if (ageCache[name]) {
+                setAge(ageCache[name])
+                return
+            }
+            const { age } = await fetch(`https://api.agify.io?name=${name}`, { signal: controller.signal }).then<AgifyResponse>(res => res.json())
+            ageCache[name] = age
+            setAge(age)
+        }
+        catch (err) {
+            if (err instanceof Error && err.name == 'AbortError') return
+            console.error(error)
+            setError("Ошибка получения возраста, попробуйте ещё раз.")
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
+    return [{ data: age, error, isLoading }, refetch] as const
+}
